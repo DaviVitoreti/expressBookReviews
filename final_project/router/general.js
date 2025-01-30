a4cfdc18be227d57efae32093804e17a0d3a1458
DaviVitoreti <DaviVitoreti@outlook.com>
2025-01-30 14:44:57 -0300

    Implemented Task 1-5 functionality

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,9 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const getAllBooks = () => {
+  return books;
+};
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -12,32 +15,38 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const allBooks = getAllBooks();
+  res.send(JSON.stringify(allBooks,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const ISBN = req.params.isbn;
+  const targetBook = books[ISBN];
+  res.send(JSON.stringify(targetBook,null,4)); 
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const matchingBooks = Object.values(books).filter(
+    (book) => book.author.toLowerCase() === req.params.author.toLowerCase()
+  );
+  res.send(JSON.stringify(matchingBooks,null,4));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const matchingTitle = Object.values(books).filter(
+    (book) => book.title.toLowerCase() === req.params.title.toLowerCase()
+  );
+  res.send(JSON.stringify(matchingTitle,null,4));
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const ISBN = req.params.isbn;
+  const targetBook = books[ISBN];
+  res.send(JSON.stringify(targetBook.reviews,null,4))
 });
 
 module.exports.general = public_users;
